a28ab996da103474858a67c2b6d55ba2f0bc4d60
yustas147 <yustas@simbioz.com.ua>
2016-08-08 11:28:53 +0300

    data parse_dict_keys.csv added

--- __openerp__.py
+++ __openerp__.py
@@ -10,7 +10,7 @@
     'depends': ['base','crm','sale'],
     'data': [
         'security/security.xml',
-        'security/ir.model.access.csv','product_proxy.xml', 'menu.xml', 'imp_data.xml', 'imp_config_data.xml', 'parser.xml'
+        'security/ir.model.access.csv', 'data/parse_dict_keys.csv','product_proxy.xml', 'menu.xml', 'imp_data.xml', 'imp_config_data.xml', 'parser.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
